21a60bb7ffb1136fe145bbc6bc628a046eee4918
kohrVidae <kohrVidae@yandex.com>
2015-07-14 16:26:45 +0100

    Raddit application with fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,13 +20,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  ###Update wasn't defined in the original tutorial
+  def update
+#    @link = Link.find(params[:link_id])
+ #   @comment = @link.comments.new(comment_params)
+#    @comment.user = current_user
 
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render json: @comment, status: :ok, location: @comment }
+      else
+        format.html { render :edit}
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to @link, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to :back, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
